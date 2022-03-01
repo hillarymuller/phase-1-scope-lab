f8428c94df9a536497b6da043cb631c6aa5ef26b
Hillary Muller <hillaryaward@gmail.com>
2022-02-28 20:08:26 -0500

    commit tree 29e4e6190bfbbd4057053c766e855eb22dab5b3c

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+customerName;
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+upperCaseCustomerName();
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "Margie";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob";
+}
+changeLeastFavoriteCustomer();
